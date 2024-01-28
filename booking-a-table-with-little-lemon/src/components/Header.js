7178f21fbac3ffd328994d18d9ff0b2d35aa3f90
Khushbu Kolhe <khushbu.kolhe@archerirm.com>
2024-01-28 16:37:11 -0700

    Added Header to the project

--- booking-a-table-with-little-lemon/src/components/Header.js
+++ booking-a-table-with-little-lemon/src/components/Header.js
@@ -1,9 +1,30 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import  { Link } from 'react-router-dom'
+import bannerImg from '../images/restauranfood.jpg'
 
 const Header =()=>{
 
     return(
-        <h1>hello Header</h1>
+        <header className="header" >
+            <section>
+            <div className="banner">
+
+             <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional recipes servred with a modern twist.
+          </p>
+          <Link to="/Booking">
+           <button>Reserve the table</button>
+          </Link>
+          </div>
+          <div className="banner-img">
+            <img src={bannerImg} alt="">
+            </img>
+          </div>
+          </section>
+            </header>
     )
 
 }
